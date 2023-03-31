2a78ab3acbc13f4e26b3d0bbdb5f6d560377b149
Stanislav Soviak <Stanislav_Soviak@epam.com>
2023-03-31 19:46:52 +0300

    get movies from API

---
+++ app/src/main/java/com/example/movieapp/di/AppModule.kt
@@ -0,0 +1,30 @@
+package com.example.movieapp.di
+
+import com.example.movieapp.data.remote.MovieApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+//    const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    @Provides
+    fun baseUrl() = "https://api.themoviedb.org/3/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(baseUrl: String): MovieApi =
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MovieApi::class.java)
+
+
+}
